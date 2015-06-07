ff7fa1eb52ee67754d1d5cd76672dfe499780839
David Ackerman <david.w.ackerman@gmail.com>
2015-06-07 06:26:46 -0700

    Unignored prelude modules

--- personal/keybindings.el
+++ personal/keybindings.el
@@ -5,5 +5,7 @@
 (global-set-key (kbd "C-<left>") 'backward-word)
 (global-set-key (kbd "C-<right>") 'forward-word)
 
+(setq magit-last-seen-setup-instructions "1.4.0")
+
 (provide 'keybindings)
 ;;; keybindings.el ends here
---
+++ personal/settings.el
@@ -0,0 +1,3 @@
+(add-hook 'clojure-mode-hook #'linum-on)
+
+(scroll-bar-mode 0)
